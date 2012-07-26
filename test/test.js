bf0d6c6d5fd252cdd63b5d81ab49f94f9a5c6b40
Kristoffer Berdal <web@flexd.net>
2012-07-26 19:03:01 +0200

    Improved error handling, README update, tests.
    Error handling has been improved a bit, still very naive and will
    probably break.
    
    Gave the README some love, and there's a very very basic
    mocha.js/should.js test suite :)

--- test/test.js
+++ test/test.js
@@ -2,10 +2,13 @@ var zabbix = require ('../lib/zabbix');
 var should = require ('should');
 var assert = require ('assert');
 
+var url = 'http://zabbix.test.sparebank1.no/api_jsonrpc.php';
+var user = 'e3130';
+var password =  'zabbix';
 describe('Zabbix', function(){
     describe('Get Api version', function() {
       it('should return a valid result', function(done){
-        var client = new zabbix.Client('http://zabbix.test.sparebank1.no/api_jsonrpc.php', 'e3130', 'zabbix');
+        var client = new zabbix.Client(url, user, password);
         client.getApiVersion(function (err, resp, body) {
           if (err) throw err;
           else {
